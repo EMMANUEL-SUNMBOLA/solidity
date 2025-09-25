cbea87367276dd2496f4d29307501860678e7ba8
Emmanuel-Sunmbola <adedayoemmanuel729@gmail.com>
2025-09-25 10:35:22 +0100

    using functions from an imported contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.30;
 
 contract SimpleStorage {
     // uint256[] listOfFavNum;
--- extra.sol
+++ extra.sol
@@ -1,16 +1,36 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.18;
+pragma solidity ^0.8.30;
 
-import {mappingstruct} from './SimpleStorage.sol';
+import {SimpleStorage} from './SimpleStorage.sol';
 
 contract defi{
 
-    mappingstruct public mpstrctD;
+    SimpleStorage public mystorageList;
 
-    function pushstrct() public{
+    function createSorage() public{
 
-        
+        SimpleStorage mystorage = new SimpleStorage();
+
+        mystorageList.push(mystorage);
+
+    }
+
+    function getCa(uint256 _index) public view returns(address){
+
+        return address(mystorageList[_index]);
+
+    }
+
+    function storeFave(uint256 _index, uint256 x) public{
+
+        mystorageList[_index].store(x);
+
+    }
+
+    function getFave(uint256 _index) public view returns(uint256){
+
+        return mystorageList[_index].retrieveFavNum();
 
     }
 
